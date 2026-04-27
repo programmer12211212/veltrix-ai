f6ecdaf2d501c1b19409e2fb4176deb5cb638f37
programmer12211212 <uchqunbekfarxodov003@example.com>
2026-04-27 22:53:47 +0500

    reset static cache fix

--- tolibov_ai/settings.py
+++ tolibov_ai/settings.py
@@ -17,7 +17,7 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
